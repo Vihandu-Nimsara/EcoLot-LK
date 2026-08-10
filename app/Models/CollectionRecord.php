b2da90f847700b27916c96c50041a5338d7e4ab8
Vihandu-Nimsara <nimsara.kav@gmail.com>
2026-08-10 19:28:36 +0530

    feat: implement initial database schema and define primary keys for core models

--- app/Models/CollectionRecord.php
+++ app/Models/CollectionRecord.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 final class CollectionRecord extends Model
 {
     protected string $table = 'collection_records';
+    protected string $primaryKey = 'collection_record_id';
 }
